6cd14bf52ce56fff63b379eba78fc149cdd99911
viv <vivin.abraham@ibm.com>
2020-10-18 15:40:27 -0500

    adding secret reference in deployment

--- src/main/java/com/example/demo/UserResource.java
+++ src/main/java/com/example/demo/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserResource {
 
+    @Value("${app.version}")
+    private String version;
+
+    @Value("${db.password}")
+    private String password;
+
     @GetMapping("hello/{name}")
     public String hello(@PathVariable("name") String name) {
         System.out.println("Service invoked by :" + name);
+        System.out.println("Password : " + password);
 
-        return "Hey " + name + "!";
+        return "Hey " + name + "!" + "  version - " + version;
     }
 
 }
